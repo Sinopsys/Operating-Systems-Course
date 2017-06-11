83517c5b7a0d27b32ce15502660f07c8f5858a4c
Sinopsys <kupriyanovkirill@gmail.com>
2017-06-11 18:10:37 +0300

    Shell reminder task.

--- Homeworks/sh/new-shell-remainder.sh
+++ Homeworks/sh/new-shell-remainder.sh
@@ -16,7 +16,7 @@ _help () {
     echo "	-c Set a command to (default - echo wake up!)"
     echo "	-m Set a message to (default - \"Wake up!\")"
     echo "	-t Set timer for this amount of 's/m/h/d' (default - 5s)" 
-    echo "	--beep Beep on notification (default - false)" 
+    echo "	-date Set a notification to pop up on a particular TIME 'HH:MM:SS'" 
     echo "	-h Help"
     exit 0
 }
@@ -27,9 +27,13 @@ init_vars () {
     message="Wake-up!"
     cmd="echo wake up!"
     i=1
-    beep=false
-    cmd_flag=false;
-    mes_flag=false;
+    cmd_flag=false
+    mes_flag=false
+    t_flag=false
+    new_date=235959
+    date_flag=false
+    current_date=$(date +%T | sed -e s/://g)
+    echo $current_date
 
     if [ $argc -ge 1 ]
     then
@@ -38,24 +42,35 @@ init_vars () {
 	    case ${argv[$i]} in
 		-c ) cmd=${argv[$(($i+1))]} cmd_flag=true;;
                 -m ) message=${argv[$(($i+1))]} mes_flag=true;;
-		-t ) r_time=${argv[$(($i+1))]};;
-		--beep ) beep=true;;
+		-t ) r_time=${argv[$(($i+1))]} t_flag=true;;
+        -date ) new_date=${argv[$(($i+1))]} date_flag=true;;
 		-h ) _help;;
 	    esac
 	    i=$(($i+2))
 	done
     fi
-
+    new_date=`echo $new_date | sed -e s/://g`
+    echo $new_date
    
 }
 
 init_vars
 
-sleep ${r_time}
+if $time_flag; then sleep ${r_time}; fi
+
+echo $new_date
+if $date_flag;
+    then
+	while [[ $current_date -ge $new_date ]];
+	do
+            current_date=$(date +%T | sed -e s/://g)
+	done;
+fi
+	
+
 
 if $mes_flag; then notify-send -t 10000 $message; fi
 if $cmd_flag; then $cmd; fi
-if $beep; then echo beep; fi
 
 # EOF
 
